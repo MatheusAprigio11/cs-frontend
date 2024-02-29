7d586e6cb42e2d2b3e7acc76231c3558de98e334
Matheus Aprigio de Jesus <44933006806>
2024-02-29 16:52:38 -0300

    updt cad screen

--- src/pages/cadastrar/index.jsx
+++ src/pages/cadastrar/index.jsx
@@ -1,17 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
 
 const Cadastro = () => {
-  
-  const navigation = useNavigation();
+	const [telefone, setTelefone] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordF, setPasswordF] = useState("");
 
-  return (
-    <View>
-      <Text>Cadastro</Text>
-      <Text onPress={() => navigation.navigate('Login')}>Vorta mofi</Text>
-    </View>
-  )
-}
+	const navigation = useNavigation();
 
-export default Cadastro
+	return (
+		<View style={styles.container}>
+      	<Text onPress={() => navigation.navigate("Login")}>Voltar </Text>
+			<View style={styles.containerSplash}>
+				<Text style={{ fontFamily: "itim", fontSize: 60, color: "#E36588" }}>
+					Clean
+				</Text>
+				<Text style={{ fontFamily: "itim", fontSize: 60, color: "#449DD1" }}>
+					Shop
+				</Text>
+			</View>
+			<View style={styles.loginspace}>
+				<View style={styles.inputcpfcnpj}>
+					<Text style={styles.textsinputs}>Telefone</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={(text) => setTelefone(text)}
+						value={telefone}
+					/>
+				</View>
+				<View>
+					<Text style={styles.textsinputs}>Senha</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={(text) => setPassword(text)}
+						value={password}
+					/>
+				</View>
+				<View>
+					<Text style={styles.textsinputs}>Confirmar Senha</Text>
+					<TextInput
+						style={styles.input}
+						onChangeText={(text) => setPasswordF(text)}
+						value={passwordF}
+					/>
+				</View>
+				<View style={styles.buttonOkView}>
+					<TouchableOpacity style={styles.btnOk}>
+						<Text style={{ fontFamily: "itim", fontSize: 30, color: "#FFF" }}>
+							Logar
+						</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+			<View>
+			
+			</View>
+		</View>
+	);
+};
+
+export default Cadastro;
