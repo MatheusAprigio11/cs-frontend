61413f69b66e64d10160ed22e36bcda94e949f5e
Matheus Aprigio de Jesus <44933006806>
2024-04-04 13:59:41 -0300

    :sparkles: Add inputfilter integrated w api

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TextInput } from "react-native";
 import React, { useEffect, useState } from "react";
 import Cards from "../../components/Cards/Cards";
 import styles from "./styles";
@@ -6,10 +6,16 @@ import instanceML from "../../api/axiosInstanceML";
 
 const Home = () => {
 	const [produtos, setProdutos] = useState([]);
+	const [filterItens, setFilterItens] = useState("Produtos de limpeza");
+
+
+	if (filterItens === "") {
+		setFilterItens("Produtos de limpeza")
+	}
 
 	const fetchData = async () => {
 		try {
-			const response = await instanceML.get("produtos de limpeza");
+			const response = await instanceML.get(filterItens);
 			setProdutos(response.data.results);
 		} catch (error) {
 			console.log(error.response.data);
@@ -18,11 +24,16 @@ const Home = () => {
 
 	useEffect(() => {
 		fetchData();
-	}, []);
+	}, [filterItens]);
 
 	return (
 		<ScrollView style={styles.containerHome}>
 			<View style={styles.testingsomething}>
+			<TextInput
+				style={styles.inputFilter}
+				onChangeText={(text) => setFilterItens(text)}
+				placeholder="Pesquise aqui o produto"
+			/>
 				{produtos.map((produto, index) => (
 					<Cards
 						key={index}
--- src/pages/home/styles.jsx
+++ src/pages/home/styles.jsx
@@ -2,8 +2,18 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   containerHome:{
-
+    backgroundColor: '#fff',
   },
+
+  inputFilter: {
+		backgroundColor: "#E8E8E8",
+		borderRadius: 12,
+		height: 40,
+		width: "90%",
+		fontFamily: "regular",
+		paddingLeft: 8,
+    marginBottom: 20,
+	},
 
   testingsomething:{
     justifyContent: 'center',
